8e4f62d247a8fe365c69778f159c80880dd58aa6
Alpha Shuro <alphashuro@gmail.com>
2016-03-10 12:00:08 +0200

    init videos app

--- interesting_youtube_videos/settings.py
+++ interesting_youtube_videos/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
+    'videos.apps.VideosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
