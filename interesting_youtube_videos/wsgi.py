aa49b0198f952c7485762301b9d43533d3b2553f
Alpha Shuro <alphashuro@gmail.com>
2016-03-10 10:44:28 +0200

    init project

---
+++ interesting_youtube_videos/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for interesting_youtube_videos project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interesting_youtube_videos.settings")
+
+application = get_wsgi_application()
