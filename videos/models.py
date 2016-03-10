f2f3aead6bde66cb5ae9a9a9bb924b02bd4e8ae1
Alpha Shuro <alphashuro@gmail.com>
2016-03-10 19:01:36 +0200

    create model

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.url
