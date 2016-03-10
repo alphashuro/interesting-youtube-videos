36d07b93f84803e052f1f3590553fae33cb86346
Alpha Shuro <alphashuro@gmail.com>
2016-03-10 09:06:13 +0200

    add webpack and xo

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+const BundleTracker = require('webpack-bundle-tracker');
+
+const options = {
+  context: __dirname,
+
+  entry: './assets/js/entry', // entry point into app
+
+  output: {
+    path: path.resolve('./assets/bundles/'),
+    filename: '[name]-[hash].js'
+  },
+
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}) // extract information for django to load the right bundle
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel' // transform jsx and es6 into js
+      }
+    ]
+  }
+};
+
+module.exports = options;
