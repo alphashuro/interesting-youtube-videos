08c48b68d4e770c43c048d8596cef6fd5850aac6
Alpha Shuro <alphashuro@gmail.com>
2016-03-10 09:33:28 +0200

    switch from spaces to tabs

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,28 @@ const path = require('path');
 const BundleTracker = require('webpack-bundle-tracker');
 
 const options = {
-  context: __dirname,
+	context: __dirname,
 
-  entry: './assets/js/entry', // entry point into app
+	entry: './assets/js/entry', // entry point into app
 
-  output: {
-    path: path.resolve('./assets/bundles/'),
-    filename: '[name]-[hash].js'
-  },
+	output: {
+		path: path.resolve('./assets/bundles/'),
+		filename: '[name]-[hash].js'
+	},
 
-  plugins: [
-    new BundleTracker({filename: './webpack-stats.json'}) // extract information for django to load the right bundle
-  ],
+	plugins: [
+		new BundleTracker({filename: './webpack-stats.json'}) // extract information for django to load the right bundle
+	],
 
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel' // transform jsx and es6 into js
-      }
-    ]
-  }
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel' // transform jsx and es6 into js
+			}
+		]
+	}
 };
 
 module.exports = options;
