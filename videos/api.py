08f4de74c1b42fe4bc2b14adc91ba2b096b8940f
Alpha Shuro <alphashuro@gmail.com>
2016-03-12 14:54:22 +0200

    add public post, put and delete

--- videos/api.py
+++ videos/api.py
@@ -1,7 +1,9 @@
 from tastypie.resources import ModelResource
 from .models import Video
+from tastypie.authorization import Authorization
 
 
 class VideoResource(ModelResource):
     class Meta:
         queryset = Video.objects.all()
+        authorization = Authorization()
