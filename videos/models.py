5d9ae2fcb1330768d8f2d93707aed254e57d53e2
Alpha Shuro <alphashuro@gmail.com>
2016-03-12 14:32:42 +0200

    require unique url

--- videos/models.py
+++ videos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Video(models.Model):
     title = models.CharField(max_length=50, blank=True)
     description = models.TextField(blank=True)
-    url = models.URLField()
+    url = models.URLField(unique=True)
 
     def __str__(self):
         return self.url
