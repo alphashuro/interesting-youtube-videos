9bfc4f4a44fd2ea692eaeeba893e4c1e991116ab
Alpha Shuro <alphashuro@gmail.com>
2016-03-14 05:13:18 +0200

    log redux actions

--- assets/js/store/index.js
+++ assets/js/store/index.js
@@ -1,11 +1,14 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 
+const logger = createLogger();
 const store = createStore(
 	rootReducer,
 	applyMiddleware(
-		thunk // allows us return functions from action creators
+		thunk, // allows us return functions from action creators
+		logger
 	),
 	// redux dev tools
 	// window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument()
